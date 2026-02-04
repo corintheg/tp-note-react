5acc5163a65e84cf0008e22efc7558ccbc7f8324
corinthe <corinthe.guerain@gmail.com>
2026-02-04 15:43:28 +0100

    Etape 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import {Routes, Route} from 'react-router'
+import Navbar from "./components/Navbar/Navbar.jsx";
 import Home from "./pages/Home/Home.jsx";
+import Collection from "./pages/Collection/Collection.jsx";
 import GameDetails from "./pages/GameDetails/GameDetails.jsx";
+import DeveloperDetails from "./pages/DeveloperDetails/DeveloperDetails.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/collection" element={<Collection/>} />
         <Route path="/games/:id" element={<GameDetails />} />
+        <Route path="/developers/:id" element={<DeveloperDetails />} />
       </Routes>
     </>
   )
